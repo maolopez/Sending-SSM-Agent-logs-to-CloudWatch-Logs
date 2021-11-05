6f4c53ee90b1d8f47e0fe3ea2ddc62c14a16b58b
maolopez1 <maurizio.school@gmail.com>
2021-11-05 10:48:55 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Sending-SSM-Agent-logs-to-CloudWatch-Logs
-quick fix for Sending SSM Agent logs to CloudWatch Logs
+<h1>#Sending SSM Agent logs to CloudWatch Logs</h1>
+
+
+General description:
+Quick fix in python. You can place the script inot an SSM Automation.
+
+References:
+
+https://docs.aws.amazon.com/systems-manager/latest/userguide/monitoring-ssm-agent.html
+
+
+https://github.com/aws/amazon-ssm-agent/blob/mainline/seelog_windows.xml.template
+
+
+https://stackoverflow.com/questions/3095434/inserting-newlines-in-xml-file-generated-via-xml-etree-elementtree-in-python
+
+
+
+
---
+++ main.py
@@ -0,0 +1,31 @@
+from xml.etree.ElementTree import ElementTree, tostring
+import xml.etree.ElementTree as ET
+import subprocess
+
+command = 'cp -p /etc/amazon/ssm/seelog.xml.template /etc/amazon/ssm/seelog.xml'
+process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+
+def indent(elem, level=0):
+    i = "\n" + level*"    "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "    "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+content = b'<seelog type="adaptive" mininterval="2000000" maxinterval="100000000" critmsgcount="500" minlevel="info"><exceptions><exception filepattern="test*" minlevel="error"/></exceptions><outputs formatid="fmtinfo"><console formatid="fmtinfo"/><rollingfile type="size" filename="{{LOCALAPPDATA}}\Amazon\SSM\Logs\{{EXECUTABLENAME}}.log" maxsize="30000000" maxrolls="5"/><filter levels="error,critical" formatid="fmterror"><rollingfile type="size" filename="{{LOCALAPPDATA}}\Amazon\SSM\Logs\errors.log" maxsize="10000000" maxrolls="5"/></filter><custom name="cloudwatch_receiver" formatid="fmtdebug" data-log-group="your-CloudWatch-log-group-name"/></outputs><formats><format id="fmterror" format="%Date %Time %LEVEL [%FuncShort @ %File.%Line] %Msg%n"/><format id="fmtdebug" format="%Date %Time %LEVEL [%FuncShort @ %File.%Line] %Msg%n"/><format id="fmtinfo" format="%Date %Time %LEVEL %Msg%n"/></formats></seelog>'
+root = ET.fromstring(content)
+indent(root)
+tree = ET.ElementTree(root)
+
+with open(f"seelog.xml", 'w+'):
+    tree.write("seelog.xml", encoding="utf-8", xml_declaration=True)
+
