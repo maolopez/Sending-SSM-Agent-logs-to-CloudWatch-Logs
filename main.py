6304fd32e3371451224ab499cf55f31d16f1e87c
maolopez <maurizio.school@gmail.com>
2021-11-11 09:44:18 -0500

    Adding a variable representing the new logs file to be added

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from xml.etree.ElementTree import ElementTree, tostring
 import xml.etree.ElementTree as ET
 import subprocess
 
-command = 'cp -p /etc/amazon/ssm/seelog.xml.template /etc/amazon/ssm/seelog.xml'
+command = 'cp -p seelog.xml.template seelog.xml'
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
+
+add_line = 'my-line'
 
 def indent(elem, level=0):
     i = "\n" + level*"    "
@@ -21,8 +23,10 @@ def indent(elem, level=0):
         if level and (not elem.tail or not elem.tail.strip()):
             elem.tail = i
 
-content = b'<seelog type="adaptive" mininterval="2000000" maxinterval="100000000" critmsgcount="500" minlevel="info"><exceptions><exception filepattern="test*" minlevel="error"/></exceptions><outputs formatid="fmtinfo"><console formatid="fmtinfo"/><rollingfile type="size" filename="{{LOCALAPPDATA}}\Amazon\SSM\Logs\{{EXECUTABLENAME}}.log" maxsize="30000000" maxrolls="5"/><filter levels="error,critical" formatid="fmterror"><rollingfile type="size" filename="{{LOCALAPPDATA}}\Amazon\SSM\Logs\errors.log" maxsize="10000000" maxrolls="5"/></filter><custom name="cloudwatch_receiver" formatid="fmtdebug" data-log-group="your-CloudWatch-log-group-name"/></outputs><formats><format id="fmterror" format="%Date %Time %LEVEL [%FuncShort @ %File.%Line] %Msg%n"/><format id="fmtdebug" format="%Date %Time %LEVEL [%FuncShort @ %File.%Line] %Msg%n"/><format id="fmtinfo" format="%Date %Time %LEVEL %Msg%n"/></formats></seelog>'
-root = ET.fromstring(content)
+content = '<seelog type="adaptive" mininterval="2000000" maxinterval="100000000" critmsgcount="500" minlevel="info"><exceptions><exception filepattern="test*" minlevel="error"/></exceptions><outputs formatid="fmtinfo"><console formatid="fmtinfo"/><rollingfile type="size" filename="{{LOCALAPPDATA}}\Amazon\SSM\Logs\{{EXECUTABLENAME}}.log" maxsize="30000000" maxrolls="5"/><filter levels="error,critical" formatid="fmterror"><rollingfile type="size" filename="{{LOCALAPPDATA}}\Amazon\SSM\Logs\errors.log" maxsize="10000000" maxrolls="5"/></filter><custom name="cloudwatch_receiver" formatid="fmtdebug" data-log-group="{}"/></outputs><formats><format id="fmterror" format="%Date %Time %LEVEL [%FuncShort @ %File.%Line] %Msg%n"/><format id="fmtdebug" format="%Date %Time %LEVEL [%FuncShort @ %File.%Line] %Msg%n"/><format id="fmtinfo" format="%Date %Time %LEVEL %Msg%n"/></formats></seelog>'.format(add_line)
+res = bytes(content, 'utf-8')
+
+root = ET.fromstring(res)
 indent(root)
 tree = ET.ElementTree(root)
 
